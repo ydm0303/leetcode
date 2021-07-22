94d0c31a7bb6729c8d7fbce188cf98c8d1fe6e1d
ydm <ydm@123.com>
2021-07-22 17:25:08 +0800

    leetcode IDEA测试提交

---
+++ src/java/Test.java
@@ -0,0 +1,13 @@
+package java;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author YDM
+ * @Date 2021/7/22 17:23
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
