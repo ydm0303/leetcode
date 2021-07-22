011d8e26bfae742a9ea7b36247a011b97ebd1e25
ydm <ydm@123.com>
2021-07-22 16:47:48 +0800

    leetcode第一次刷题

---
+++ src/java/leetcode_01.java
@@ -0,0 +1,34 @@
+package java;
+
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * @ClassName leetcode_01
+ * @Description TODO
+ * @Author YDM
+ * @Date 2021/7/22 16:25
+ **/
+public class leetcode_01 {
+    class Solution{
+        public int[] twoSum(int[] nums,int target){
+            int[] result = new int[] {0,1};
+            if(nums.length == 2){
+                return result;
+            }
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1 ; j < nums.length; j++) {
+                    if(nums[i] + nums[j] ==target){
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+
+                    }
+                }
+
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ src/java/leetcode_02.java
@@ -0,0 +1,12 @@
+package java;
+
+/**
+ * @ClassName leetcode_02
+ * @Description TODO
+ * @Author YDM
+ * @Date 2021/7/22 16:39
+ **/
+public class leetcode_02 {
+
+
+}
