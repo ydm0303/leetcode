8783a2e68ba9fe8ed05f1ab4d1cbd5c03f47ad68
ydm <ydm@123.com>
2021-07-23 10:13:51 +0800

    检查是否区域覆盖

---
+++ src/java/leetcode_03.java
@@ -0,0 +1,23 @@
+package java;
+
+/**
+ * @ClassName leetcode_03
+ * @Description TODO
+ * @Author YDM
+ * @Date 2021/7/23 10:04
+ **/
+class leetcode_03 {
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        int[] a = new int[51];
+        for(int i = 0; i < ranges.length; i++)
+            for(int j = ranges[i][0]; j <= ranges[i][1]; j++)
+                a[j] = 1;
+        for(int i = left; i <= right; i++)
+            if(a[i] != 1)
+                return false;
+        return true;
+    }
+}
+
+
+
